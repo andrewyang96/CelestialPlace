fb677bdffa43d1997b6e1b03368be00f081c45a9
Andrew Yang <andrewyang96@gmail.com>
2018-02-24 01:04:52 -0600

    Make grid update channels

---
+++ lib/celestial_web/channels/grid_channel.ex
@@ -0,0 +1,8 @@
+defmodule Celestial.GridChannel do
+  use CelestialWeb, :channel
+  alias Celestial.Grid
+
+  def join("grid", _params, socket) do
+    {:ok, %{grid: Grid.list_grid_squares()}, socket}
+  end
+end
