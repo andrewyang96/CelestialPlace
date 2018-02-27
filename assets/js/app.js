14529e82d53bb62fe3d809d31c58061814ea6e7f
Andrew Yang <andrewyang96@gmail.com>
2018-02-27 02:00:42 -0600

    Bamboozled myself by transposing row-col and x-y coords

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,14 +8,18 @@ import svgPanZoom from "svg-pan-zoom";
   const gridWidth = 800, gridHeight = 800;
   const draw = SVG('grid').size(800, 800);
 
+  const colorFill = (draw, row, col, rgbColor) => {
+    return draw.rect(7, 7).move(7 * col + 1, 7 * row + 1).fill('#' + rgbColor);
+  }
+
   const makeGrid = (draw, gridColors, rowSpan, colSpan) => {
     const grid = [];
     for (let row = 0; row < 100; row++) {
       grid[row] = [];
       for (let col = 0; col < 100; col++) {
         grid[row][col] = {};
-        grid[row][col].border = draw.rect(9, 9).move(7 * row, 7 * col).fill('white');
-        grid[row][col].square = draw.rect(7, 7).move(7 * row + 1, 7 * col + 1).fill('#' + gridColors[row][col]);
+        grid[row][col].border = draw.rect(9, 9).move(7 * col, 7 * row).fill('white');
+        grid[row][col].square = colorFill(draw, row, col, gridColors[row][col]);
         grid[row][col].square.on('mouseover', function (row, col) {
           rowSpan.textContent = row;
           colSpan.textContent = col;
