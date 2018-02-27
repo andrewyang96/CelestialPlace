1db17d9c52faa8a05f4abb5e581c5936c323ff63
Andrew Yang <andrewyang96@gmail.com>
2018-02-27 01:32:07 -0600

    Basic socket functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ import "phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+import "./socket"
 
 import SVG from "svg.js"
 import svgPanZoom from "svg-pan-zoom"
@@ -56,4 +56,6 @@ import svgPanZoom from "svg-pan-zoom"
     zoomScaleSensitivity: 0.1,
     controlIconsEnabled: true
   });
+
+  window.createGridSocket();
 })();
